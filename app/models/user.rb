ddc7f4f0ebe58b26cb92eb1172f3e5fceec9edf1
Eduardo Cordova <eduucordova@gmail.com>
2017-03-10 20:56:58 -0300

    back to ldap authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :ldap_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable, password_length: 4..40
+  devise :ldap_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 4..40
 
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable, password_length: 4..40
+  # devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable, password_length: 4..40
 
   has_many :users_protocols, dependent: :destroy
   has_many :protocols, through: :users_protocols
 
-  # def ldap_before_save
-  #   self.email = Devise::LDAP::Adapter.get_ldap_param(self.username,"mail").first
-  # end
+  def ldap_before_save
+    self.email = Devise::LDAP::Adapter.get_ldap_param(self.username,"mail").first
+  end
 
 end
