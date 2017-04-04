3a280967989a8812375ca9d0f319e208de39febb
Eduardo Cordova <eduucordova@gmail.com>
2017-04-04 10:41:11 -0300

    deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :passenger_restart_with_touch, true
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/eduardo/Workspace/TCC-SLR'
+set :deploy_to, '/home/luiz/SLR'
 
 # Default value for :scm is :git
 # set :scm, :git
